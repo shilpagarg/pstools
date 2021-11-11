a7a4c8bf2e49660495af5fbb64c6374a0279f963
Jun Aruga <jaruga@redhat.com>
2021-11-11 16:01:22 +0100

    Add resolve_haplotypes test.
    We don't add the testing data files to this repository for now.
    
    To run the resolve_haplotypes test,
    
    1. Set the test data into `test/data/resolve_haplotypes/`, referring the
      `test/run.sh`.
    2. Modify the `TEST_COMMANDS` test/run.sh to run the `resolve_haplotypes` test.
    3. Then run `make test`.
    4. To run with `gdb`, run `TEST_DEBUG=1 make test`

--- test/run.sh
+++ test/run.sh
@@ -6,14 +6,34 @@ ROOT_DIR=$(pwd)
 popd
 
 TARGET="${ROOT_DIR}/pstools"
+# TEST_COMMANDS="qv resolve_haplotypes"
+TEST_COMMANDS="qv"
+# TEST_COMMANDS="resolve_haplotypes"
+# Debug option.
+TEST_DEBUG=${TEST_DEBUG-}
+GDB_ARGS=
+if [ -n "${TEST_DEBUG}" ]; then
+    GDB_ARGS='gdb -q -ex "set breakpoint pending on" --args'
+fi
 
 set -eux
 
+test_qv() {
+    # The command returns non-zero even when the command is success.
+    "${TARGET}" qv ./true_hap2.fasta ./hap2.1.fastq ./hap2.2.fastq > qv_score.txt || :
+    grep QV_RAW -A 1 qv_score.txt
+    grep -E '^19\.628\s+-1\.000' qv_score.txt
+}
+
+test_resolve_haplotypes() {
+    rm -rf out
+    ${GDB_ARGS} "${TARGET}" resolve_haplotypes -t 8 ./hic_name_connection.output ./icHarAxyr1.r_utg.gfa out
+}
+
 "${TARGET}" 2>&1 | grep -q Usage
 
-pushd test/data/qv
-# The command returns non-zero even when the command is success.
-"${TARGET}" qv ./true_hap2.fasta ./hap2.1.fastq ./hap2.2.fastq > qv_score.txt || :
-grep QV_RAW -A 1 qv_score.txt
-grep -E '^19\.628\s+-1\.000' qv_score.txt
-popd
+for cmd in ${TEST_COMMANDS}; do
+    pushd test/data/${cmd}
+    test_${cmd}
+    popd
+done
